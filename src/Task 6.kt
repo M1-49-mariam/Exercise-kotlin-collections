6a5361424a024652e980fe66163f49b7417e7b04
M1-49-mariam <mariam.almutairi19@gmail.com>
2025-02-13 18:34:55 +0300

    finished exercise

---
+++ src/Task 6.kt
@@ -0,0 +1,7 @@
+val listNumbers = listOf(2.5,7.1,3.8,9.2,5.6)
+
+fun main() {
+
+    println("Largest number: ${listNumbers.maxOrNull()}")
+    println("Smallest number: ${listNumbers.minOrNull()}")
+}

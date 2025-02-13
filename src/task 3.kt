a8138cd324ef1077ae37eee5bc8214d98b0c83c3
M1-49-mariam <mariam.almutairi19@gmail.com>
2025-02-13 17:54:44 +0300

    task 3

--- src/task 3.kt
+++ src/task 3.kt
@@ -1,4 +1,12 @@
-
+val countries = mutableMapOf("USA" to "Washington", "France" to "Paris", "Germany" to "Berlin")
 fun main() {
+    countries.put("Kuwait", "Kuwait City")
+    val countriesValues = countries.values
+    val countriesKeys = countries.keys
+
+    println("Countries: $countriesKeys")
+    println("Capitals: $countriesValues")
+    countries.remove("France")
+    println("Final Map: $countries")
 
 }

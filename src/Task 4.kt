fa7b5d91a699c565c555e33f28b09b519ce1b10b
M1-49-mariam <mariam.almutairi19@gmail.com>
2025-02-13 17:59:48 +0300

    task 4

---
+++ src/Task 4.kt
@@ -0,0 +1,9 @@
+val carBrands = arrayListOf("BMW", "Toyota", "Honda")
+
+fun main() {
+
+    carBrands.add("Ford")
+    carBrands.add("Lincoln")
+    carBrands.remove("Honda")
+    println(carBrands)
+}
